0cbbe20436d1e10b02b1fea5bd53354d90f0ee90
Ashab-al <asxabal7@gmail.com>
2024-07-06 09:36:50 +0300

    added routes and controller api::shops

--- app/controllers/api/shops_controller.rb
+++ app/controllers/api/shops_controller.rb
@@ -1,2 +1,18 @@
 class Api::ShopsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def categories
+    render json: Category.all
+  end
+
+  def products
+    render json: Product.all
+  end
 end
